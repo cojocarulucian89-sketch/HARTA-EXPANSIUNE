0f1f78da411b7680ecff9a4552eccec59472cf66
cojocarulucian89-sketch <cojocarulucian89@gmail.com>
2026-02-06 16:22:39 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-// config.js (va fi public pe GitHub Pages)
-window.PPLX_API_KEY = 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;
+// config.js (public pe GitHub Pages)
+window.PPLX_API_KEY = "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";
 window.PPLX_MODEL = "sonar-pro";
